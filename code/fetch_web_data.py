db251e5404055fb33c8b51d29bebc0498aee963d
ioannisk <john.karamanlakis@gmail.com>
2017-06-21 14:10:29 +0100

    m

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -20,6 +20,6 @@ df['url'] = ["".join(["http://www.", u]) if u[:4] != "www." else "".join(["http:
 # print(len(df['url']))
 for i in range(10):
     page = storage.get_page(df['url'][i])
-    print(page)
+    print(df['url'][i])
     page.links
 # print(page.links)
