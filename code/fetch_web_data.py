50ded0a857043dfefa2ef84e42edf31d402726bd
ioannisk <john.karamanlakis@gmail.com>
2017-06-21 16:46:30 +0100

    first vectorizer version

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -1,6 +1,10 @@
 from evolutionai import StorageEngine
+from  sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
 import pandas as pd
 import re
+
+
 
 def clean_up_txt(page_txt):
     page_txt = re.sub('\s+',' ',page_txt)
@@ -8,8 +12,23 @@ def clean_up_txt(page_txt):
     return page_txt
 
 
+# Check this out interesting:
+# http://scikit-learn.org/stable/modules/feature_extraction.html
+def make_vocabulary(data_frame):
+    web_sites = []
+    for i in range(len(data_frame)):
+        # query database and get page object
+        page = storage.get_page(data_frame['url'][i])
+        page_txt = page.textSummary
+        page_txt = clean_up_txt(page_txt)
+        web_sites.append(page_txt)
+    vectorizer = CountVectorizer(min_df=1, stop_words=stopWords)
+    vectorizer.fit(web_sites)
 
 
+
+# English stopwords
+stopWords = stopwords.words('english')
 # get path to the database
 storage = StorageEngine("/nvme/webcache/")
 # read the domains.tsv file in pandas
@@ -24,15 +43,7 @@ for ver in df["vertical"]:
     label_txt.append(label_t)
 df["label_num"] = label_num
 df["label_txt"] = label_txt
-# format the urls so the Storage Engine can be queried
-# df['url'] = ["".join(["http://www.", u]) if u[:4] != "www." else "".join(["http://", u]) for u in df['url']]
-# print(len(df['url']))
-for i in range(10):
-    # query database and get page object
-    page = storage.get_page(df['url'][i])
-    page_txt = page.textSummary
-    page_txt = clean_up_txt(page_txt)
-    print(page_txt)
+make_vocabulary(df)
 
 
 
