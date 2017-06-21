de30d393330c3654df21dbfcef1eeccfe2ca99fb
ioannisk <john.karamanlakis@gmail.com>
2017-06-21 12:04:22 +0100

    silly typos

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -8,5 +8,5 @@ storage = StorageEngine("/nvme/webcache/")
 companies_df = pd.read_csv('../data/domains.tsv', sep='\t', names = ["company_name", "company_id", "url", "vertical"])
 companies_df['url'] = ["".join(["http://www.", u]) if u[:4] != "www." else "".join(["http://", u]) for u in companies_df['url']]
 
-page = s.get_page(url)
+page = storage.get_page(url)
 print(page.links)
