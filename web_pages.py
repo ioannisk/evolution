7bbdb33446663387a1cc94401efc5b60043cdabb
ioannisk <john.karamanlakis@gmail.com>
2017-06-21 11:39:08 +0100

    directory update

---
+++ web_pages.py
@@ -0,0 +1,9 @@
+from evolutionai import StorageEngine
+storage = StorageEngine("/nvme/webcache/")
+
+
+companies_df = pd.read_csv('../data/domains.tsv', sep='\t', names = ["company_name", "company_id", "url", "vertical"])
+companies_df['url'] = ["".join(["http://www.", u]) if u[:4] != "www." else "".join(["http://", u]) for u in companies_df['url']]
+
+page = s.get_page(url)
+print(page.links)
