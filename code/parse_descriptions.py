feebb703ce01073a961408f4f0e5b491a2b7fec0
ioannisk <john.karamanlakis@gmail.com>
2017-06-22 14:53:03 +0100

    adding descriptions parser

--- code/parse_descriptions.py
+++ code/parse_descriptions.py
@@ -2,15 +2,9 @@ import pandas as pd
 import json
 
 df = pd.read_csv('../data/sic_descriptons.tsv', sep='\t', names = ["class_num", "class_txt", "json"])
-# for i in df['json']:
-#     print(i)
+json_formatter = lambda x: json.loads(x)
+df['json'] = df['json'].apply(json_formatter)
 
-print("###########")
-print("###########")
-print("###########")
-print("###########")
-print("###########")
-print("###########")
-df.join(df['json'].apply(json.loads).apply(pd.Series))
-for i in df['json']:
-    print(i['title'])
+# df.join(df['json'].apply(json.loads).apply(pd.Series))
+# for i in df['json']:
+#     print(i['title'])
