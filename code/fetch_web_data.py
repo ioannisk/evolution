8efaf0ab6c044db666e4a76972bf8bd6712c9241
ioannisk <john.karamanlakis@gmail.com>
2017-06-22 12:48:53 +0100

    vectorization

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -53,7 +53,11 @@ df["label_num"] = label_num
 df["label_txt"] = label_txt
 web_sites = []
 print("Fetch websites from database")
+counter = 0
 for i in df['url']:
+    counter +=1
+    if counter > 10000:
+        break
     # query database and get page object
     page = storage.get_page(i)
     # some domains are not scrapped
