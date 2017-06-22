971cc8c3ad8fa4a626d0f4931fe9ce75a7a809d3
ioannisk <john.karamanlakis@gmail.com>
2017-06-22 12:47:46 +0100

    vectorization

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -5,8 +5,9 @@ from collections import defaultdict
 from nltk.corpus import stopwords
 import pandas as pd
 import re
-
-
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('GTK')
 ## META
 
 ## TITLE and DESCRIPTION
@@ -69,9 +70,9 @@ gnb = GaussianNB()
 d = defaultdict(int)
 for i in df['label_num']:
     d[i]+=1
-
+plt.bar(d.keys(), d.values(), width, color='g')
 print("Train Naive Bayes")
-y_pred = gnb.fit(data, df["label_num"]).predict(data)
+y_pred = gnb.fit(data.toarray(), df["label_num"]).predict(data)
 
 
 
