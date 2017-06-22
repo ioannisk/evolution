4169da4055dabc67ad5675bff20de551d1d6ad12
ioannisk <john.karamanlakis@gmail.com>
2017-06-22 17:16:26 +0100

    working on parsing discr

--- code/parse_descriptions.py
+++ code/parse_descriptions.py
@@ -9,6 +9,14 @@ import json
 #
 ################################
 
+def check_json_keys(df):
+    keys = set()
+    for entry in df['json']
+        keys.update(set(entry.keys()))
+    print(keys)
+
+
+
 df = pd.read_csv('../data/sic_descriptons.tsv', sep='\t', names = ["class_num", "class_txt", "json"])
 json_formatter = lambda x: json.loads(x)
 df['json'] = df['json'].apply(json_formatter)
@@ -17,12 +25,12 @@ count = 0
 for txt, js in  zip(df["class_txt"], df["json"]):
     print(txt)
     for key in js:
-        if key=="title":
+        if key=="":
             count +=1
         print(key + "::::",js[key])
     print("\n\n\n")
 print(count)
-
+check_json_keys(df)
 
 # # df.join(df['json'].apply(json.loads).apply(pd.Series))
 # for i in df['json']:
