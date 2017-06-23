5d85269af3899cf8a816918b48553bcb17f71226
ioannisk <john.karamanlakis@gmail.com>
2017-06-23 14:48:05 +0100

    experiments

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -73,7 +73,7 @@ for i in df['label_num']:
 classes = [(key, d[key])for key in d]
 print(classes)
 
-stop
+# stop
 # plt.bar(d.keys(), d.values(), width=1.0, color='g')
 
 
@@ -117,7 +117,7 @@ des_web_sites = des_data + web_sites
 des_web_sites_labels =  des_labels + labels
 data_len = len(des_web_sites)
 partition = int(data_len*0.9)
-vec = CountVectorizer(min_df=20,ngram_range=(1,2), stop_words=stopWords)
+vec = CountVectorizer(alpha=0.1, min_df=20,ngram_range=(1,2), stop_words=stopWords)
 des_web_sites = vec.fit_transform(des_web_sites)
 train_X = des_web_sites[:partition]
 train_y = des_web_sites_labels[:partition]
@@ -139,7 +139,7 @@ print("Testing accuracy (web + des)trainging (web) testing: {0}".format(accuracy
 print("Train Naive Bayes")
 data_len = len(web_sites)
 partition = int(data_len*0.9)
-vec = CountVectorizer(min_df=20, ngram_range=(1,2),stop_words=stopWords)
+vec = CountVectorizer(alpha = 0.1, min_df=20, ngram_range=(1,2),stop_words=stopWords)
 data = vec.fit_transform(web_sites)
 train_X = data[:partition]
 train_y = labels[:partition]
