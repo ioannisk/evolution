2cb96d1140469ece63ba61d9740bb443735d55d9
ioannisk <john.karamanlakis@gmail.com>
2017-06-26 10:50:12 +0100

    experiments

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -109,10 +109,6 @@ df["label_txt"] = label_txt
 
 intersection =find_intersection_of_classes()
 print(len(des_df))
-print(type(des_df["class_num"][0]))
-for i in intersection:
-    print(type(i))
-    fovjnfo
 des_df = des_df[~des_df["class_num"].isin(intersection)]
 
 print(len(des_df))
