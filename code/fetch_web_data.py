45a3ae3592c328bce5452ebc1b9306531e2181dd
ioannisk <john.karamanlakis@gmail.com>
2017-06-26 11:10:31 +0100

    experiments

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -222,8 +222,8 @@ des_data = vec.transform(des_data)
 print(len(web_sites))
 # print(vec.vocabulary_)
 print(des_data.shape)
-for a in np.arange(0.001,1,0.1):
-    gnb = MultinomialNB(alpha=0.1)
+for a in np.arange(0.0001,1.4,0.05):
+    gnb = MultinomialNB(alpha=a)
     clf = gnb.fit(des_data, des_labels)
     vec_web_sites = vec.transform(web_sites)
     y_pred_test = clf.predict(vec_web_sites)
