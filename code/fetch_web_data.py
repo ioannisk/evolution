61178778fcb7ab3de208b9f2bae91abe7a2aadc5
ioannisk <john.karamanlakis@gmail.com>
2017-06-27 10:04:18 +0100

    error analysis

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -177,9 +177,9 @@ all_urls = []
 print("Fetch websites from database")
 counter = 0
 for i, l, c_id in zip(df['url'], df["label_num"], df["company_id"]):
-    counter +=1
-    if counter > 10000:
-        break
+    # counter +=1
+    # if counter > 10000:
+    #     break
     # query database and get page object
     page = storage.get_page(i)
     # some domains are not scrapped
