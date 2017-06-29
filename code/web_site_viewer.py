213d6a8f037a6fec440ef9982426c8164d0a64be
ioannisk <john.karamanlakis@gmail.com>
2017-06-29 13:53:54 +0100

    edit viewer

--- code/web_site_viewer.py
+++ code/web_site_viewer.py
@@ -188,8 +188,8 @@ while True:
     print("#####################")
     # try:
     row = df_wrong.loc[df_wrong['company_id'] == int(var)]
-    label =row["label"]
-    pred = row["pred"]
+    label =row["label"][0]
+    pred = row["pred"][0]
     print(label, pred)
     # print(dic[var])
     # except:
