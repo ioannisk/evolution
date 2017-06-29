ac73ed955d35d06526db66f6c186b68591fe7674
ioannisk <john.karamanlakis@gmail.com>
2017-06-29 15:36:14 +0100

    edit viewer

--- code/fetch_web_data.py
+++ code/fetch_web_data.py
@@ -131,7 +131,7 @@ for des_json, cl_num in zip(des_df['json'], des_df["class_num"]):
     ##
     if cl_num == 72110:
         # print("lol")
-        print des_json.keys()
+        print(des_json.keys())
         stop
     if ("detail" in des_json.keys()) or ("includes" in des_json.keys()) :
         used_classes.add(cl_num)
