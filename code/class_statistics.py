1039d1a31ea43cde3419c8c74da21a3f1521e3f9
ioannisk <john.karamanlakis@gmail.com>
2017-07-03 11:14:32 +0100

    stats

--- code/class_statistics.py
+++ code/class_statistics.py
@@ -1,12 +1,20 @@
 from evolutionai import StorageEngine
 import pandas as pd
 from collections import Counter
+from itertools import dropwhile
 
 df = pd.read_csv('../data/domains.tsv', sep='\t', names = ["company_name", "company_id", "url", "vertical"])
 count_companies = Counter()
 for id_ in df["vertical"]:
     count_companies[id_] +=1
+
+for key, count in dropwhile(lambda key_count: key_count[1] >= 2, sorted_companies.most_common()):
+    del sorted_companies[key]
+
+
 sorted_companies = sorted(count_companies.items(), key=lambda pair: pair[1], reverse=True)
 for i in sorted_companies:
     print(i[0], i[1])
+
+
 print(len(sorted_companies))
