c5fe69ffa16ab9f886ba68c43bab4b6cf3ca8c49
ioannisk <john.karamanlakis@gmail.com>
2017-07-03 11:09:05 +0100

    stats

--- code/class_statistics.py
+++ code/class_statistics.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 df = pd.read_csv('../data/domains.tsv', sep='\t', names = ["company_name", "company_id", "url", "vertical"])
 count_companies = Counter()
-for id_ in df["company_id"]:
+for id_ in df["vertical"]:
     count_companies[id_] +=1
 sorted_companies = sorted(count_companies.items(), key=lambda pair: pair[1], reverse=True)
 for i in sorted_companies:
