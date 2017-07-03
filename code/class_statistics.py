94e2d2d3f0d17467fb46927f25d2dcb8f5421143
ioannisk <john.karamanlakis@gmail.com>
2017-07-03 11:19:47 +0100

    stats

--- code/class_statistics.py
+++ code/class_statistics.py
@@ -3,19 +3,16 @@ import pandas as pd
 from collections import Counter
 from itertools import dropwhile
 
+N = 10
+
 df = pd.read_csv('../data/domains.tsv', sep='\t', names = ["company_name", "company_id", "url", "vertical"])
 count_companies = Counter()
 for id_ in df["vertical"]:
     count_companies[id_] +=1
-count_companies = dict(count_companies)
-
-for key, count in dropwhile(lambda key_count: key_count[1] >= 20, count_companies.items()):
-    del count_companies[key]
-
-
 sorted_companies = sorted(count_companies.items(), key=lambda pair: pair[1], reverse=True)
+more_than_dic = {}
 for i in sorted_companies:
-    print(i[0], i[1])
-
-
-print(len(sorted_companies))
+    if sorted_companies[i] >= N:
+        more_than_dic[i] = sorted_companies[i]
+    # print(i[0], i[1])
+print(len(more_than_dic))
