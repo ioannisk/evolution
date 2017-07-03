e3294a8d8be08e5af1b648493def63a940a4c81f
ioannisk <john.karamanlakis@gmail.com>
2017-07-03 11:22:30 +0100

    stats

--- code/class_statistics.py
+++ code/class_statistics.py
@@ -10,9 +10,11 @@ count_companies = Counter()
 for id_ in df["vertical"]:
     count_companies[id_] +=1
 sorted_companies = sorted(count_companies.items(), key=lambda pair: pair[1], reverse=True)
-more_than_dic = {}
+more_than = []
 for i in sorted_companies:
-    if sorted_companies[i] >= N:
-        more_than_dic[i] = sorted_companies[i]
-    # print(i[0], i[1])
+    if i[1] >= N:
+        more_than.append(i)
+
+for i in more_than:
+    print(i[0], i[1])
 print(len(more_than_dic))
