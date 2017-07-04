e381caf673186c18bac82e9f640853a66c6bd3b9
ioannisk <john.karamanlakis@gmail.com>
2017-07-04 19:35:20 +0100

    doc2vec

--- code/doc2vec.py
+++ code/doc2vec.py
@@ -20,6 +20,81 @@ from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import normalize
 
 
+
+
+def clean_up_txt(page_txt):
+    page_txt = page_txt.lower()
+    page_txt = re.sub('\s+',' ',page_txt)
+    page_txt = re.sub('[^0-9a-zA-Z]+', " ", page_txt)
+    return page_txt
+
+# def remove_url_chars(string):
+#     for ch in ['/','.', '_', '#', '-']:
+#         string = string.lower()
+#         if ch in string:
+#             string=string.replace(ch," ")
+#     return string
+
+
+#################################
+#
+# Statistics about popularity of classes
+#
+#################################
+def n_most_popular_classes(N):
+    d = defaultdict(int)
+    norm = 0
+    # count how many times each class appears
+    for i in df['label_num']:
+        norm += 1
+        d[i]+=1
+    # number of class at pos 0 name in counts at position 1
+    classes = [(key, d[key])for key in d]
+    # sort classes according to popularity
+    classes.sort(key=lambda tup: tup[1], reverse=True)
+    total_percentage = 0
+    list_of_n_classes = []
+    list_of_n_classes_txt = []
+    independent_percentages = []
+    ## make dictionairy that given the class number it return the class name
+    class_hash = {num:txt for num, txt in zip(df["label_num"], df["label_txt"])}
+    for i in range(N):
+        total_percentage += classes[i][1]*100/norm
+        independent_percentages.append(classes[i][1]*100/norm)
+        # print(class_hash[classes[i][0]], classes[i][1]*100/norm)
+        list_of_n_classes.append(classes[i][0])
+        list_of_n_classes_txt.append(class_hash[classes[i][0]])
+    top_n_classes = zip(list_of_n_classes, independent_percentages)
+    return (top_n_classes, total_percentage)
+# print(cksum)
+
+#################################
+#
+# Remove exclusions from descriptions so text can be used as training data
+#
+#################################
+def get_descriptions_data(des_df):
+    des_data = []
+    for des_json in des_df['json']:
+        valid_txt = ""
+        for key in des_json:
+            if key!="excludes":
+                valid_txt += " "+des_json[key][0]
+        valid_txt = clean_up_txt(valid_txt)
+        des_data.append(valid_txt)
+    return des_data
+
+
+def find_intersection_of_classes():
+    # ["class_num", "class_txt", "json"]
+    classes_desc = set(des_df["class_num"])
+    classes_web = set(df["label_num"])
+    intersection = classes_desc.intersection(classes_web)
+    return intersection
+
+
+
+
 # English stopwords
 stopWords = stopwords.words('english')
 # get path to the database
