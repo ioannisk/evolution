4564cd57bc753b0297b5a2d3b05ab5c6475926df
ioannisk <john.karamanlakis@gmail.com>
2017-07-04 20:13:43 +0100

    doc2vec

--- code/doc2vec.py
+++ code/doc2vec.py
@@ -19,7 +19,8 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import normalize
 
-
+from os import listdir
+from os.path import isfile, join
 
 
 def clean_up_txt(page_txt):
@@ -241,22 +242,42 @@ df_web["urls"]=all_urls
 print("Labeled websites are {0}".format(len(df_web)))
 
 
+#
+#   Prepare the data
+#
 
-for id_, txt in zip(df_web["company_id"], df_web["summaries"]):
-    f = open("doc2vec_data/{0}.txt".format(id_), "w")
-    f.write(txt)
+# for id_, txt in zip(df_web["company_id"], df_web["summaries"]):
+#     f = open("doc2vec_data/{0}.txt".format(id_), "w")
+#     f.write(txt)
 
 
-stop
+# stop
 class LabeledLineSentence(object):
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, doc_list, labels_list):
+       self.labels_list = labels_list
+       self.doc_list = doc_list
     def __iter__(self):
-        for uid, line in enumerate(open(filename)):
-            yield LabeledSentence(words=line.split(), labels=['SENT_%s' % uid])
+        for idx, doc in enumerate(self.doc_list):
+            yield LabeledSentence(words=doc.split(),labels=[self.labels_list[idx]])
 
-documents = list(df_web["class_txt"])
-documents_raw = list(df_web["summaries"])
+docLabels = []
+docLabels = [f for f in listdir("doc2vec_data") if f.endswith('.txt')]
+data = []
+for doc in docLabels:
+    data.append(open("doc2vec_data/" + doc, ‘r’)
+it = DocIt.DocIterator(data, docLabels)
+
+model = gensim.models.Doc2Vec(size=300, window=10, min_count=5, workers=11,alpha=0.025, min_alpha=0.025) # use fixed learning rate
+model.build_vocab(it)
+for epoch in range(10):
+    print(epoch)
+    model.train(it)
+    model.alpha -= 0.002 # decrease the learning rate
+    model.min_alpha = model.alpha # fix the learning rate, no deca
+    model.train(it)
+
+# documents = list(df_web["class_txt"])
+# documents_raw = list(df_web["summaries"])
 
 # for i in range(10):
 #     print('------------------')
