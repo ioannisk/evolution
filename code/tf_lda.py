9dab7fef62dcd4750c5794e8cbe98f204b2b0dd4
ioannisk <john.karamanlakis@gmail.com>
2017-07-06 15:01:35 +0100

    update utilities

--- code/tf_lda.py
+++ code/tf_lda.py
@@ -2,31 +2,70 @@ import tensorflow as tf
 import pickle
 import numpy as np
 
+########################################################
+# Data importing
+########################################################
 lda_data = pickle.load(open("../models/topics/lda_data.pckl","rb"))
 des_data = pickle.load(open("../models/topics/des_vectors.pckl","rb"))
 web_data = pickle.load(open("../models/topics/web_vectors.pckl","rb"))
 
-
 lda_vectors = lda_data['vectors']
-lda_vectors = np.asarray([vec.reshape([1,10]) for vec in lda_vectors])
 lda_labels = lda_data['labels']
 
 des_vec = des_data['vectors'].todense()
 des_labels = des_data['labels']
 
-# print(lda_data)
 web_vec = web_data['vectors'].todense()
 web_labels = web_data['labels']
 
 
-print(des_vec.shape)
-print(lda_vectors.shape)
-for i,j in zip(des_vec, lda_vectors):
-    # j = j.reshape([1,10])
-    print(i.shape, j.shape)
+a = zip(des_vec, lda_vectors)
+print(len(a))
+print(len(a[0]))
+print((a[0][0].shape))
+print((a[0][1].shape))
+
+########################################################
+# Tensorflow model
+########################################################
+LEARNING_RATE = 0.5
+BATCH_SIZE = 10
+
+voc_size = des_vec.shape[1]
+lda_topics = lda_data.shape[1]
+
+x = tf.placeholder(tf.float32, [None, voc_size])
+y = tf.placeholder(tf.float32,[None,lda_topics])
+
+W = tf.get_variable(name='W',shape=[voc_size, lda_topics])
+b = tf.get_variable(name='b', shape=[1,lda_topics])
+pred = tf.matmul(x,W) + b
+
+loss = tf.reduce_sum(tf.square(y - pred))
+optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+########################################################
 
 
-x = tf.placeholder(tf.float32, [None, 784])
-y_true = tf.placeholder(tf.float32,[None,10])
 
-W = tf.get_variable(name='W',shape=[784, 128])
+for
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
