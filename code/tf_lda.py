9992d9341c03c2961c12a43ee6437017c6c8fa3a
ioannisk <john.karamanlakis@gmail.com>
2017-07-06 18:32:51 +0100

    update utilities

--- code/tf_lda.py
+++ code/tf_lda.py
@@ -35,8 +35,8 @@ data = list(zip(des_vec, lda_vectors))
 ########################################################
 # Tensorflow model
 ########################################################
-LEARNING_RATE = 0.0001
-BATCH_SIZE = 60
+LEARNING_RATE = 0.001
+BATCH_SIZE = 320
 EPOCHS = 500
 HIDDEN = 100
 
