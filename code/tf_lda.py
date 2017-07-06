43f58bdc1e66151243f39e40f3b1ef30857ecf76
ioannisk <john.karamanlakis@gmail.com>
2017-07-06 16:25:11 +0100

    update utilities

--- code/tf_lda.py
+++ code/tf_lda.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn import linear_model
 
 ########################################################
 # Data importing
@@ -61,21 +62,31 @@ sess.run(init)
 ########################################################
 # Training
 ########################################################
-for l in [ 5, 10, 15, 20, 25, 50]:
+for l in [0.001, 0.1, 1, 5, 10, 15, 20, 25, 50]:
     clf = KNeighborsClassifier(n_neighbors=1)
     clf.fit(lda_vectors, lda_labels)
     # l = 0
-    print("lambda {0}".format(l))
-    for i in range(EPOCHS):
-        # print("epoch {0}".format(i))
-        epoch_cost = 0.0
-        for j in range(0,len(data),BATCH_SIZE):
-            train_x = des_vec[j:j+BATCH_SIZE]
-            train_y = lda_vectors[j:j+BATCH_SIZE]
-            _, cost = sess.run([optimizer, loss], feed_dict={x:train_x, y:train_y, lamb:l})
-            epoch_cost += cost
-    print("cost is {0}".format(epoch_cost/len(data)))
-    tf_pred = sess.run(pred, feed_dict={x:web_vec})
+    ################################
+    # TF model
+    ################################
+    # print("lambda {0}".format(l))
+    # for i in range(EPOCHS):
+    #     # print("epoch {0}".format(i))
+    #     epoch_cost = 0.0
+    #     for j in range(0,len(data),BATCH_SIZE):
+    #         train_x = des_vec[j:j+BATCH_SIZE]
+    #         train_y = lda_vectors[j:j+BATCH_SIZE]
+    #         _, cost = sess.run([optimizer, loss], feed_dict={x:train_x, y:train_y, lamb:l})
+    #         epoch_cost += cost
+    # print("cost is {0}".format(epoch_cost/len(data)))
+    # tf_pred = sess.run(pred, feed_dict={x:web_vec})
+    ################################
+    # scikit model
+    ################################
+    reg = linear_model.Ridge (alpha = l)
+    reg.fit(des_vec, lda_vectors)
+    tf_pred = reg.predict(web_vec)
+
     n_pred = clf.predict(tf_pred)
     print("NB acc {0}".format(accuracy_score( web_labels,n_pred, normalize=True)*100))
     print()
