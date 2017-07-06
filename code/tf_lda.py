14565fc6facdc91411e5fa11a809c24a642e928c
ioannisk <john.karamanlakis@gmail.com>
2017-07-06 12:28:01 +0100

    update utilities

---
+++ code/tf_lda.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import pickle
+
+lda_data = pickle.load(open("../models/topics/lda_data.pckl","rb"))
+des_data = pickle.load(open("../models/topics/des_vectors.pckl","rb"))
+web_data = pickle.load(open("../models/topics/web_vectors.pckl","rb"))
+
+lda_vectors = lda_data['vectors']
+lda_labels = lda_data['labels']
+
+des_vec = des_data['vectors']
+des_labels = des_data['labels']
+
+web_vec = web_data['vectors']
+web_labels = web_data['labels']
