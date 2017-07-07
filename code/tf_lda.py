4d583362dcf9ce7e837451502690d2ef263f80cb
ioannisk <john.karamanlakis@gmail.com>
2017-07-07 12:46:26 +0100

    update utilities

--- code/tf_lda.py
+++ code/tf_lda.py
@@ -35,13 +35,21 @@ data = list(zip(des_vec, lda_vectors))
 ########################################################
 # Tensorflow model
 ########################################################
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
 LEARNING_RATE = 0.001
 BATCH_SIZE = 50
 EPOCHS = 10
 HIDDEN = 50
 
-voc_size = des_vec.shape[1]
-lda_topics = lda_vectors.shape[1]
+
+voc_size = 784
+lda_topics = 10
+# voc_size = des_vec.shape[1]
+# lda_topics = lda_vectors.shape[1]
 
 x = tf.placeholder(tf.float32, [None, voc_size])
 y = tf.placeholder(tf.float32,[None,lda_topics])
@@ -65,15 +73,26 @@ b1 = tf.get_variable(name='b1', shape=[1,HIDDEN])
 b2 = tf.get_variable(name='b2', shape=[1,lda_topics])
 
 h1 = tf.nn.sigmoid(tf.matmul(x,W1) + b1)
-pred = tf.matmul(h1,W2) + b2
 
-square_error = tf.reduce_sum(tf.square(y - pred))
-regularizer = tf.nn.l2_loss(W1) + tf.nn.l2_loss(W2)
-loss = square_error + lamb*regularizer
 
-# pred =tf.nn.softmax(tf.matmul(x,W) + b)
-# cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=[1]))
-# loss = cross_entropy
+# pred = tf.matmul(h1,W2) + b2
+
+# square_error = tf.reduce_sum(tf.square(y - pred))
+# regularizer = tf.nn.l2_loss(W1) + tf.nn.l2_loss(W2)
+# loss = square_error + lamb*regularizer
+
+pred =tf.nn.softmax(tf.matmul(x,W) + b)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=[1]))
+loss = cross_entropy
+
+
+
+correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+
+
 
 optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
 init = tf.global_variables_initializer()
@@ -87,29 +106,34 @@ clf.fit(lda_vectors, lda_labels)
 # Training
 ########################################################
 # LEARNING_RATE
-for l in [1, 10, 15, 20, 25, 50]:
+# for l in [1, 10, 15, 20, 25, 50]:
     # l = 0
     ################################
     # TF model
     ################################
-    print("lambda {0}".format(l))
-    for i in range(EPOCHS):
-        # print("epoch {0}".format(i))
-        epoch_cost = 0.0
-        for j in range(0,len(data),BATCH_SIZE):
-            train_x = des_vec[j:j+BATCH_SIZE]
-            train_y = lda_vectors[j:j+BATCH_SIZE]
-            _, cost = sess.run([optimizer, loss], feed_dict={x:train_x, y:train_y, lamb:l, lr:LEARNING_RATE})
-            epoch_cost += cost
-        # print("epoch_cost is {0}".format(epoch_cost/(len(data))))
-        LEARNING_RATE *= 0.99
-    print("cost is {0}".format(epoch_cost/len(data)))
-    tf_pred = sess.run(pred, feed_dict={x:des_vec})
-    tf_pred_test = sess.run(pred, feed_dict={x:web_vec})
+    # print("lambda {0}".format(l))
+    # for i in range(EPOCHS):
+    #     # print("epoch {0}".format(i))
+    #     epoch_cost = 0.0
+    #     for j in range(0,len(data),BATCH_SIZE):
+    #         train_x = des_vec[j:j+BATCH_SIZE]
+    #         train_y = lda_vectors[j:j+BATCH_SIZE]
+    #         _, cost = sess.run([optimizer, loss], feed_dict={x:train_x, y:train_y, lamb:l, lr:LEARNING_RATE})
+    #         epoch_cost += cost
+    #     # print("epoch_cost is {0}".format(epoch_cost/(len(data))))
+    #     LEARNING_RATE *= 0.99
+    # print("cost is {0}".format(epoch_cost/len(data)))
+    # tf_pred = sess.run(pred, feed_dict={x:des_vec})
+    # tf_pred_test = sess.run(pred, feed_dict={x:web_vec})
     # stop
     ################################
     # scikit model
     ################################
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys, lamb:0})
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
 
     # reg = linear_model.Ridge(alpha = l)
     # reg.fit(des_vec, lda_vectors)
@@ -118,12 +142,12 @@ for l in [1, 10, 15, 20, 25, 50]:
     # rmse = np.mean(np.square(tf_pred-lda_vectors))
     # print("RMSE  {0}".format(rmse))
 
-    print(sum(tf_pred[0]))
-    n_pred = clf.predict(tf_pred)
-    n_pred_test = clf.predict(tf_pred_test)
-    print("NB TRAINING acc {0}".format(accuracy_score( n_pred,des_labels, normalize=True)*100))
-    print("NB TESTING acc {0}".format(accuracy_score( n_pred_test,web_labels, normalize=True)*100))
-    print()
+    # print(sum(tf_pred[0]))
+    # n_pred = clf.predict(tf_pred)
+    # n_pred_test = clf.predict(tf_pred_test)
+    # print("NB TRAINING acc {0}".format(accuracy_score( n_pred,des_labels, normalize=True)*100))
+    # print("NB TESTING acc {0}".format(accuracy_score( n_pred_test,web_labels, normalize=True)*100))
+    # print()
     # stop
 # regression with L2 l=10 1-nn has accuracy of 0.85%
 
