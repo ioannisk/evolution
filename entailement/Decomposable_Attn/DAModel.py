ec9933ad18dd0632ef16774aaab066bb83826d68
ioannisk <john.karamanlakis@gmail.com>
2017-07-12 16:37:02 +0100

    fix repo

--- entailement/Decomposable_Attn/DAModel.py
+++ entailement/Decomposable_Attn/DAModel.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 # from tensorflow.python.ops import  rnn, seq2seq
-from tensorflow.python.ops import seq2seq
+from tensorflow.python.ops import basic_rnn_seq2seq
 from embedding_utils import input_projection3D
 
 class DAModel(object):
@@ -115,7 +115,7 @@ class DAModel(object):
 
         _, targets = tf.nn.top_k(self.targets)
 
-        loss = seq2seq.sequence_loss_by_example(
+        loss = basic_rnn_seq2seq.sequence_loss_by_example(
                 [self.logits],
                 [targets],
                 [tf.ones([batch_size])],
