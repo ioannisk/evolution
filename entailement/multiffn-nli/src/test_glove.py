696988e3f07f96ecbfc7b834c51806cdd6269acc
ioannisk <john.karamanlakis@gmail.com>
2017-07-14 13:40:00 +0100

    update

--- entailement/multiffn-nli/src/test_glove.py
+++ entailement/multiffn-nli/src/test_glove.py
@@ -2,5 +2,7 @@
 with open("/home/ioannis/data/glove/glove.840B.300d.txt", "r") as file_ :
     for line in file_:
         line = line.strip().split()
-        print len(line)
-        rcricn
+        if len(line) != 301:
+            print line[0]
+        # print len(line)
+        # rcricn
