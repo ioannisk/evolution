0139d8111db62ee255da55721563b42dcf386cd6
ioannisk <john.karamanlakis@gmail.com>
2017-07-14 13:40:54 +0100

    update

--- entailement/multiffn-nli/src/test_glove.py
+++ entailement/multiffn-nli/src/test_glove.py
@@ -1,6 +1,9 @@
-
+counter = 0
 with open("/home/ioannis/data/glove/glove.840B.300d.txt", "r") as file_ :
     for line in file_:
+        counter +=1
+        if counter%100000==0:
+            print counter
         line = line.strip().split()
         if len(line) != 301:
             print line[0]
