144d251f38a0332b310122a9212255cb742243d7
ioannisk <john.karamanlakis@gmail.com>
2017-07-14 13:39:25 +0100

    update

--- entailement/multiffn-nli/src/test_glove.py
+++ entailement/multiffn-nli/src/test_glove.py
@@ -1,6 +1,5 @@
 
-
-with file_ as open("/home/ioannis/data/glove/glove.840B.300d.txt", "r"):
+with open("/home/ioannis/data/glove/glove.840B.300d.txt", "r") as file_ :
     for line in file_:
         line = line.strip().split()
         print len(line)
