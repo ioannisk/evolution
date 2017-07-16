0abc992499cc39ebe8c161b299fefad78f0f6d11
ioannisk <john.karamanlakis@gmail.com>
2017-07-16 20:06:41 +0100

    wrt locally

--- code/wrt_locally.py
+++ code/wrt_locally.py
@@ -9,7 +9,8 @@ def wrt_dataframes():
             file_.write("{0}\t{1}\n".format(class_num, txt))
     with open("../data/web_site_data.txt", "w") as file_:
         for txt, class_num, id_ in zip(df_web["class_txt"], df_web["class_num"],df_web["company_id"]):
-            file_.write("{0}\t{1}\t{2}\n".format(id_, class_num, txt))
+            if txt is not None:
+                file_.write("{0}\t{1}\t{2}\n".format(id_, class_num, txt))
 
     # pickle.dump(des_df, open("../data/des_df.pkl","wb"), protocol=2)
     # pickle.dump(df_web, open("../data/df_web.pkl","wb"), protocol=2)
