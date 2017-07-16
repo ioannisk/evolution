eae0e2f53b57838a4f4113fb812f136427226354
ioannisk <john.karamanlakis@gmail.com>
2017-07-16 19:13:18 +0100

    wrt locally

---
+++ code/make_attention_dataset.py
@@ -0,0 +1,10 @@
+import pickle
+import pandas
+import json
+#### data -> dictionairy -> json
+
+
+df_web = pickle.load( open("../data/df_web.pkl","rb"))
+des_df = pickle.load(open("../data/des_df.pkl","rb"))
+
+
--- code/wrt_locally.py
+++ code/wrt_locally.py
@@ -4,8 +4,15 @@ import pickle
 
 def wrt_dataframes():
     des_df, df_web = data_pipeline()
-    pickle.dump(des_df, open("../data/des_df.pkl","wb"), protocol=2)
-    pickle.dump(df_web, open("../data/df_web.pkl","wb"), protocol=2)
+    with open("../data/descriptions_data.txt","w") as file_:
+        for txt, class_num in zip(des_df["txt"], des_df["class_num"]):
+            file_.write("{0}\t{1}\n".format(class_num, txt))
+    with open("../data/web_site_data.txt". "w") as file_:
+        for txt, class_num in zip(df_web["class_txt"], df_web["class_num"])
+            file_.write("{0}\t{1}\n".format(class_num, txt))
+
+    # pickle.dump(des_df, open("../data/des_df.pkl","wb"), protocol=2)
+    # pickle.dump(df_web, open("../data/df_web.pkl","wb"), protocol=2)
 
 if __name__=="__main__":
     wrt_dataframes()
