5fa372d94f04a330cbce64139b841de30a5e3a83
ioannisk <john.karamanlakis@gmail.com>
2017-07-16 20:01:55 +0100

    wrt locally

--- code/make_attention_dataset.py
+++ code/make_attention_dataset.py
@@ -10,7 +10,9 @@ import random
 
 def read_data():
     des = {}
-    web = {}
+    web_class = {}
+    id_txt = {}
+    id_class = {}
     with open("../data/descriptions_data.txt","r") as file_:
         for line in file_:
             line = line.strip()
@@ -20,16 +22,41 @@ def read_data():
         for line in file_:
             line = line.strip()
             id_, class_num , txt = line.split('\t')
-            web[class_num] = txt
-    return des, web
+
+            if class_num in web_class:
+                web_class[class_num].append((id_, txt))
+            else:
+                web_class[class_num] = []
+                web_class[class_num].append((id_, txt))
+            id_txt[id_] = txt
+            id_class[id_] = class_num
+    return des, web_class, id_txt, id_class
 
 
-def make_pairs(des, web):
+def make_pairs(des, web_class, id_txt, id_class):
     positive = []
-    for key in web
+    negative = []
+    ids = set(id_txt.keys())
+    classes = set(des.keys())
+    for class_num in web_class:
+        des_txt = des[class_num]
+        for id_, web_txt in web_class[class_num]:
+            positive.append(des_txt, web_txt)
+    for i in range(len(positive)):
+        # take one random sample (sample is return as a list so take 0 element)
+        id_ = random.sample(ids, 1)[0]
+        unsuccesful_sample = True
+        while unsuccesful_sample:
+            cl = random.sample(classes,1)[0]
+            unsuccesful_sample = False
+            if id_class[id_]==cl:
+                unsuccesful_sample = True
+        negative.append(des[cl], id_txt[id_])
+
 
 
 
 if __name__ =="__main__":
-    des, web  = read_data()
+    des, web_class, id_txt, id_class  = read_data()
+    make_pairs(des, web_class, id_txt, id_class)
 
