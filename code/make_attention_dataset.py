c3fa19707279585083cc9c488f54844027fae35b
ioannisk <john.karamanlakis@gmail.com>
2017-07-17 10:35:35 +0100

    wrt locally

--- code/make_attention_dataset.py
+++ code/make_attention_dataset.py
@@ -47,16 +47,21 @@ def make_pairs(des, web_class, id_txt, id_class):
         des_txt = des[class_num]
         for id_, web_txt in web_class[class_num]:
             positive.append((des_txt, web_txt))
+    counter = 0
     for i in range(len(positive)):
+        print len(positive)
+        counter +=1
+        if counter % 1000 ==0:
+            print counter
         # take one random sample (sample is return as a list so take 0 element)
         id_ = random.sample(ids, 1)[0]
         unsuccesful_sample = True
         while unsuccesful_sample:
             cl = random.sample(classes,1)[0]
-            print id_
-            print cl
-            print id_class[id_]
-            necn
+            # print id_
+            # print cl
+            # print id_class[id_]
+            # necn
             # print
             if id_class[id_]==cl:
                 unsuccesful_sample = True
