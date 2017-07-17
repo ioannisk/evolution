8a722bbc19f7a10b81221be670cc7edda146c341
ioannisk <john.karamanlakis@gmail.com>
2017-07-17 12:56:46 +0100

    debug

--- entailement/multiffn-nli/src/my_train.py
+++ entailement/multiffn-nli/src/my_train.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
-# nohup python -u my_train.py ~/data/glove/glove.840B.300d.txt ~/home/ioannis/evolution/data/training_pairs.json ~/home/ioannis/evolution/data/validation_pairs.json my_model mlp --lower -e 30 -u 200 -d 0.8 --l2 0 -b 32 -r 0.05 --optim adagrad &
+# nohup python -u my_train.py ~/data/glove/glove.840B.300d.txt /home/ioannis/evolution/data/training_pairs.json /home/ioannis/evolution/data/validation_pairs.json my_model mlp --lower -e 30 -u 200 -d 0.8 --l2 0 -b 32 -r 0.05 --optim adagrad &
+#python -u my_train.py ~/data/glove/glove.840B.300d.txt /home/ioannis/evolution/data/training_pairs.json /home/ioannis/evolution/data/validation_pairs.json my_model mlp --lower -e 30 -u 200 -d 0.8 --l2 0 -b 32 -r 0.05 --optim adagrad
+#python -u my_train.py ~/data/glove/fast_glove.txt /home/ioannis/evolution/data/training_pairs.json /home/ioannis/evolution/data/validation_pairs.json my_model mlp --lower -e 30 -u 200 -d 0.8 --l2 0 -b 32 -r 0.05 --optim adagrad
 
-
-# python train.py ~/data/glove/glove.840B.300d.txt ~/data/snli_1.0/snli_1.0_train.jsonl ~/data/snli_1.0/snli_1.0_dev.jsonl test_models mlp --lower -e 30 -u 200 -d 0.8 --l2 0 -b 32 -r 0.05 --optim adagrad
-# python train.py ~/data/glove/glove.42B.300d.txt ~/data/snli_1.0/snli_1.0_train.jsonl ~/data/snli_1.0/snli_1.0_dev.jsonl test_models mlp --lower -e 30 -u 200 -d 0.8 --l2 0 -b 32 -r 0.05 --optim adagrad
 from __future__ import division, print_function
 
 """
@@ -82,6 +81,7 @@ if __name__ == '__main__':
     word_dict, embeddings = ioutils.load_embeddings(args.embeddings, args.vocab,
                                                     True, normalize=True)
 
+    print(word_dict)
     logger.info('Converting words to indices')
     # find out which labels are there in the data
     # (more flexible to different datasets)
