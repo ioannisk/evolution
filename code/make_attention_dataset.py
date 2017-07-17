0bda7595c3efb8613fa9143f8460e9e5c41ddfec
ioannisk <john.karamanlakis@gmail.com>
2017-07-17 14:02:35 +0100

    debug

--- code/make_attention_dataset.py
+++ code/make_attention_dataset.py
@@ -3,11 +3,29 @@ import pandas
 import json
 import random
 import time
+from utilities import data_pipeline
 #### data -> dictionairy -> json
 
 
 # df_web = pickle.load( open("../data/df_web.pkl","rb"))
 # des_df = pickle.load(open("../data/des_df.pkl","rb"))
+
+
+
+def wrt_dataframes():
+    des_df, df_web = data_pipeline()
+    with open("../data/descriptions_data.txt","w") as file_:
+        for txt, class_num in zip(des_df["txt"], des_df["class_num"]):
+            file_.write("{0}\t{1}\n".format(class_num, txt))
+    with open("../data/web_site_data.txt", "w") as file_:
+        for txt, class_num, id_ in zip(df_web["class_txt"], df_web["class_num"],df_web["company_id"]):
+            if txt is not "":
+                txt_buffer = ""
+                for i in txt.split():
+                    print i
+                    kfkvnf
+                # if txt.split()
+                file_.write("{0}\t{1}\t{2}\n".format(id_, class_num, txt))
 
 
 def read_data():
