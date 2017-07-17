b2348dd0a8b25d7492d8415dd596f6f4749bf4b5
ioannisk <john.karamanlakis@gmail.com>
2017-07-17 10:54:54 +0100

    wrt locally

--- code/make_attention_dataset.py
+++ code/make_attention_dataset.py
@@ -49,7 +49,8 @@ def make_pairs(des, web_class, id_txt, id_class):
             positive.append({'des':des_txt, 'web':web_txt, 'class':1})
     with open("positive_pairs.txt", 'wb') as file_:
         for i in positive:
-            json.dump(i, file_)
+            print i
+            # json.dump(i, file_)
     dcindi
     counter = 0
     print len(positive)
