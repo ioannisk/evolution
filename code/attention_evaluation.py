94173bc75f7146c8fce96b63e971255f5f4d4aa9
ioannisk <john.karamanlakis@gmail.com>
2017-07-20 18:30:37 +0100

    adding evaluation

--- code/attention_evaluation.py
+++ code/attention_evaluation.py
@@ -22,7 +22,7 @@ def make_training_corpus(file_):
         des_class = line["des_class"]
         web_class = line["web_class"]
         web_id = line["web_id"]
-        if ["class"]=="entailement":
+        if binary_class=="entailement":
             training_corpus.append(web_txt)
     return training_corpus
     # return des_txt, web_txt, binary_class, des_class, web_class, web_id
