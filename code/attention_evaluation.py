f720f22db1b9ee9bda1b2ae3ac09e046c85ad264
ioannisk <john.karamanlakis@gmail.com>
2017-07-24 17:07:20 +0100

    updare evaluation

--- code/attention_evaluation.py
+++ code/attention_evaluation.py
@@ -75,11 +75,10 @@ def tfidf_inference(des_tfidf, des_class, web_tfidf, web_class):
     print((des_tfidf.shape))
     print((web_tfidf.shape))
     print(len(des_class))
-    a  = cosine_similarity(web_tfidf[0:2], des_tfidf)
-    b = cosine_similarity(web_tfidf, des_tfidf)
-    print a==b[0:2]
+    pairwise_cos_matrix  = cosine_similarity(web_tfidf, des_tfidf)
+    # print a==b[0:2]
     # print(np.max(web_tfidf[0]))
-    print a.shape
+    print pairwise_cos_matrix.shape
     stop
     for i, (web, web_cl) in enumerate(zip(web_tfidf, web_class)):
         similarities = []
