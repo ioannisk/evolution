7c74994032f4340c40dca8a569d0eb1f4ce01937
ioannisk <john.karamanlakis@gmail.com>
2017-07-25 16:05:06 +0100

    updare evaluation

--- code/train_nb.py
+++ code/train_nb.py
@@ -23,12 +23,12 @@ def train_naive_bayes():
     # except:
     #     print(len(des_labels))
 
-    import IPython; IPython.embed()
-    X_train= vec_web_sites[:len(vec_web_sites)*0.95]
-    X_valid= vec_web_sites[len(vec_web_sites)*0.95:]
+    # import IPython; IPython.embed()
+    X_train= vec_web_sites[:(vec_web_sites.shape[0])*0.95]
+    X_valid= vec_web_sites[(vec_web_sites.shape[0])*0.95:]
 
-    Y_train = labels[:len(vec_web_sites)*0.95]
-    Y_valid = labels[len(vec_web_sites)*0.95:]
+    Y_train = labels[:(vec_web_sites.shape[0])*0.95]
+    Y_valid = labels[(vec_web_sites.shape[0])*0.95:]
 
 
     clf = gnb.fit(vec_des_data, des_labels)
