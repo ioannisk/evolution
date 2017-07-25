2c995d7b440e7d85113f7fe2cec845cb42ce00c4
ioannisk <john.karamanlakis@gmail.com>
2017-07-25 14:38:55 +0100

    updare evaluation

--- code/attention_evaluation.py
+++ code/attention_evaluation.py
@@ -172,7 +172,7 @@ def baseline_nb():
     tfidf_valid = vec.transform(x_valid)
     print tfidf_train.shape
     print tfidf_valid.shape
-    for a in (np.arange(11)*0.1):
+    for a in (np.arange(1,11)*0.1):
         gnb = MultinomialNB(alpha=a)
         print "training nb with alpha {}".format(a)
         clf = gnb.fit(tfidf_train, y_train)
