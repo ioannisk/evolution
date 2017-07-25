a6dfdfde7bfcbf9086b10cdb6252cb5bf5897e06
ioannisk <john.karamanlakis@gmail.com>
2017-07-25 09:36:35 +0100

    updare evaluation

--- code/attention_evaluation.py
+++ code/attention_evaluation.py
@@ -4,7 +4,7 @@ from nltk.corpus import stopwords
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-TOP_N = 10
+TOP_N = 5
 
 def tf_idf_vectorization(corpus):
     print("tfidf Vectorization")
