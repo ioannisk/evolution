03b545a9f55a5cb453d33e28213700fa5980b655
ioannisk <john.karamanlakis@gmail.com>
2017-07-25 16:13:46 +0100

    updare evaluation

--- code/train_nb.py
+++ code/train_nb.py
@@ -25,19 +25,25 @@ def train_naive_bayes():
 
     # import IPython; IPython.embed()
     dim =int((vec_web_sites.shape[0]))
-    split = int(dim*0.995)
+    split = int(dim*0.95)
     X_train= vec_web_sites[:split]
     X_valid= vec_web_sites[split:]
-
     Y_train = labels[:split]
     Y_valid = labels[split:]
+    clf = gnb.fit(X_train, Y_train)
+    y_pred_test = clf.predict(X_valid)
+    y_pred_train = clf.predict(X_train)
+    print("Training acc is {0}".format(accuracy_score(Y_train ,y_pred_train )))
+    print("NB Testing accuracy des - web: {0} with alpha {1}".format(accuracy_score( Y_valid,y_pred_test, normalize=True),a))
 
 
-    clf = gnb.fit(vec_des_data, des_labels)
-    y_pred_test = clf.predict(vec_web_sites)
-    y_pred_train = clf.predict(vec_des_data)
-    print("Training acc is {0}".format(accuracy_score(des_labels ,y_pred_train )))
-    print("NB Testing accuracy des - web: {0} with alpha {1}".format(accuracy_score( labels,y_pred_test, normalize=True),a))
+
+
+    # clf = gnb.fit(vec_des_data, des_labels)
+    # y_pred_test = clf.predict(vec_web_sites)
+    # y_pred_train = clf.predict(vec_des_data)
+    # print("Training acc is {0}".format(accuracy_score(des_labels ,y_pred_train )))
+    # print("NB Testing accuracy des - web: {0} with alpha {1}".format(accuracy_score( labels,y_pred_test, normalize=True),a))
 
 
 if __name__=="__main__":
