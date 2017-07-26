ab8d04dcd209e0ca8834259876270a793afe5050
ioannisk <john.karamanlakis@gmail.com>
2017-07-26 14:20:28 +0100

    fixing naive bayes

--- code/attention_evaluation.py
+++ code/attention_evaluation.py
@@ -128,7 +128,7 @@ def train_naive_bayes_des_local():
     print("validation NB data {}".format(len(X_valid)))
     # a = 1
     for a in np.arange(1,20)*0.1:
-        gnb = MultinomialNB(alpha=a)
+        gnb = MultinomialNB(alpha=a,fit_prior=False)
         clf = gnb.fit(X_train_des_vec, Y_train_des)
         y_pred_test = clf.predict(X_valid_vec)
         y_pred_train = clf.predict(X_train_vec)
