95c27008bd23547e620a3ca40ea5dffdb86229e6
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 17:31:13 +0100

    refactor dataset generation

--- code/generation_matching_dataset.py
+++ code/generation_matching_dataset.py
@@ -4,9 +4,15 @@ import random
 import time
 import os
 
-MAX_LEN=1e10
+MAX_LEN=111
 MAX_DES_LEN=MAX_LEN
 MAX_WEB_LEN=MAX_LEN
+
+def web_des_intersection(class_descriptions, companies_descriptions):
+    des_set = set(class_descriptions.keys())
+    web_set = set([companies_descriptions[key]["class_num"] for key in companies_descriptions])
+    print(len(des_set))
+    print(len(web_set))
 
 
 def read_descriptions():
@@ -21,26 +27,24 @@ def read_descriptions():
 
 
 def read_meta():
-    a = set()
+    companies_descriptions = {}
     with open("../data/web_site_meta_1.txt", "r") as file_:
         for line in file_:
             line = line.strip()
             id_, class_num , txt = line.split('\t')
-            a.add(class_num)
-    return a
-            # if class_num not in set()
+            companies_descriptions[id_] = {"class_num":class_num, "txt":txt}
+    return companies_descriptions
+
+
 
 if __name__=="__main__":
-    b = read_descriptions()
-    a = read_meta()
-    b = set(b.keys())
-    print(len(b))
-    print(len(a))
-    print( b == a )
+    class_descriptions = read_descriptions()
+    companies_descriptions = read_meta()
+    class_descriptions, companies_descriptions = web_des_intersection(class_descriptions, companies_descriptions)
+
+    # b = set(b.keys())
+    # print(len(b))
+    # print(len(a))
+    # print( b == a )
 
 
-# web_class = {}
-# id_txt = {}
-# id_class = {}
-# max_des = 0
-# des_lens = []
