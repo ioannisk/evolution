41bff9dfc3b2101ad2efb0b1f6b25c9d96aede9e
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 17:55:09 +0100

    refactor dataset generation

--- code/generation_matching_dataset.py
+++ code/generation_matching_dataset.py
@@ -10,26 +10,31 @@ MAX_WEB_LEN=MAX_LEN
 
 
 def web_des_intersection(class_descriptions, cmp_des):
-
+    """Because of lenght restrictions
+    the class_num set of web and des might not overlap.
+    This function ensures that the training data will have a
+    perfect overlap
+    """
+    print(len(cmp_des))
     des_set = set(class_descriptions.keys())
     web_set = set([cmp_des[key]["class_num"] for key in cmp_des])
-    print(len(des_set))
-    print(len(web_set))
     intersection =  des_set.intersection(web_set)
-    print(len(cmp_des))
     class_descriptions = {key:class_descriptions[key] for key in class_descriptions if key in intersection}
     cmp_des = {key:cmp_des[key] for key in cmp_des if cmp_des[key]["class_num"] in intersection}
-    print(len(cmp_des))
     des_set = set(class_descriptions.keys())
     web_set = set([cmp_des[key]["class_num"] for key in cmp_des])
+
     print(len(des_set))
     print(len(web_set))
+    print(len(cmp_des))
 
-
-
-
+    return class_descriptions, cmp_des
 
 def read_descriptions():
+    """Read the meta data file
+    input: des txt file produced from wrt_locally.py
+    output: {class_num: txt }
+    """
     class_descriptions = {}
     with open("../data/descriptions_data_1.txt","r") as file_:
         for line in file_:
@@ -39,20 +44,19 @@ def read_descriptions():
                 class_descriptions[class_num] = txt
     return class_descriptions
 
-
 def read_meta():
+    """Read the meta data file
+    input: meta txt file produced from wrt_locally.py
+    output: {id: {"class_num":, "txt": } }
+    """
     companies_descriptions = {}
-    a= set()
     with open("../data/web_site_meta_1.txt", "r") as file_:
         for line in file_:
             line = line.strip()
             id_, class_num , txt = line.split('\t')
-            companies_descriptions[id_] = {"class_num":class_num, "txt":txt}
-            a.add(class_num)
-    print(len(a))
+            if len(txt.split()) <= MAX_WEB_LEN:
+                companies_descriptions[id_] = {"class_num":class_num, "txt":txt}
     return companies_descriptions
-
-
 
 if __name__=="__main__":
     class_descriptions = read_descriptions()
