64f920177707c62379e29c422f800a2353d60f19
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 18:37:10 +0100

    refactor dataset generation

--- code/generation_matching_dataset.py
+++ code/generation_matching_dataset.py
@@ -79,7 +79,7 @@ if __name__=="__main__":
     class_descriptions = read_descriptions()
     companies_descriptions = read_meta()
     class_descriptions, companies_descriptions = web_des_intersection(class_descriptions, companies_descriptions)
-    make_N_folds_classes(class_descriptions, companies_descriptions, N=6)
+    make_N_folds_classes(class_descriptions, companies_descriptions, N=5)
 
 
 
