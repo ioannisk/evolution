171f9ce8f78ebb152724d0a00d5405393464dc61
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 15:40:42 +0100

    refactor dataset generation

---
+++ code/generation_matching_dataset.py
@@ -0,0 +1,37 @@
+import pandas
+import json
+import random
+import time
+import os
+
+MAX_LEN=111
+MAX_DES_LEN=MAX_LEN
+MAX_WEB_LEN=MAX_LEN
+
+
+def read_descriptions():
+    des = {}
+    with open("../data/descriptions_data.txt","r") as file_:
+        for line in file_:
+            line = line.strip()
+            class_num , txt = line.split('\t')
+            if len(txt.split()) <=MAX_DES_LEN:
+                des[class_num] = txt
+    return class_descriptions
+
+
+def read_meta():
+    for line in file_:
+        line = line.strip()
+        id_, class_num , txt = line.split('\t')
+
+if __name__=="__main__":
+    read_descriptions()
+    read_meta()
+
+
+# web_class = {}
+# id_txt = {}
+# id_class = {}
+# max_des = 0
+# des_lens = []
