6b763b8cd4128d856aed269a4939f7762a3b8890
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 18:33:11 +0100

    refactor dataset generation

--- code/generation_matching_dataset.py
+++ code/generation_matching_dataset.py
@@ -50,7 +50,7 @@ def web_des_intersection(class_descriptions, cmp_des):
     cmp_des = {key:cmp_des[key] for key in cmp_des if cmp_des[key]["class_num"] in intersection}
     return class_descriptions, cmp_des
 
-def make_N_folds_classes(class_descriptions, companies_descriptions, N=5):
+def make_N_folds_classes(class_descriptions, companies_descriptions, N=6):
     """ Make N datasets such that there is no
     class overlap between training and testing.
     With some additional logic for dataset balance,
