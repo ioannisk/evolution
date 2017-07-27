745e8cafcfc50e2dae5df415e009bd371e37ac90
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 20:32:04 +0100

    refactor dataset generation

--- code/generation_matching_dataset.py
+++ code/generation_matching_dataset.py
@@ -125,7 +125,7 @@ def make_N_folds_classes_equal_datapoints(class_descriptions, companies_descript
     for id_ in companies_descriptions:
         class_counts[companies_descriptions[id_]["class_num"]]+=1
     # Rank according to least common count
-    ranked = class_counts.most_common()[::-1]
+    ranked = class_counts.most_common()
     folds_volume = [0 for i in range(N)]
     folds = [[] for i in range(N)]
     # + (len(companies_descriptions)/N)*0.1
