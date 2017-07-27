1b47b8c284693cfa9f30efe30b6be8cdb6978a29
ioannisk <john.karamanlakis@gmail.com>
2017-07-27 20:18:09 +0100

    refactor dataset generation

--- code/generation_matching_dataset.py
+++ code/generation_matching_dataset.py
@@ -104,6 +104,7 @@ def allocate_bin(folds, folds_volume, class_num, counts, fold_index, app_fold_vo
     """ Recursive class bin allocator
     returns: N bin with the same amount of data points
     """
+    print("index: {}".format(fold_index))
     if folds_volume[fold_index] < app_fold_volume:
         folds[fold_index].append(class_num)
         folds_volume[fold_index] += counts
