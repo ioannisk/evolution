1b8f0207ee4d476b3f8bbf1645019a0815fade29
ioannisk <john.karamanlakis@gmail.com>
2017-07-29 17:27:17 +0100

    refactor dataset generation

--- code/evaluation.py
+++ code/evaluation.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from collections import Counter
-from generation_matching import read_descriptions, read_meta, web_des_intersection
+from generation_matching_dataset import read_descriptions, read_meta, web_des_intersection
 
 TOP_N = 1
 
