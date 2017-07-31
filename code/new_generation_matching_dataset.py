95ce59c02b53951fae24674c853b3b7079d1318b
ioannisk <john.karamanlakis@gmail.com>
2017-07-31 17:47:41 +0100

    work on various datasets

--- code/new_generation_matching_dataset.py
+++ code/new_generation_matching_dataset.py
@@ -174,7 +174,7 @@ def training_validation_split(class_descriptions,companies_descriptions):
     for id_ in companies_descriptions:
         class_counts[companies_descriptions[id_]["class_num"]]+=1
     # Rank according to least common count
-    # ranked = class_counts.most_common()[::-1]
+    print(fodls)
     for fold in folds:
         fold_sum = 0
         for class_ in fold:
