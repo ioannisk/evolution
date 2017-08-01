cdabb2d35912004ff2a22bfd566f43d663f25f5a
ioannisk <john.karamanlakis@gmail.com>
2017-08-01 11:44:46 +0100

    work on various datasets

--- code/convert_snli.py
+++ code/convert_snli.py
@@ -7,14 +7,15 @@ def write_json_line(json_ ,file_):
 
 
 
-training_snli ="/home/ioannis/data/multinli_0.9/multinli_0.9_train.jsonl"
-validation_snli ="/home/ioannis/data/multinli_0.9/multinli_0.9_dev_matched.jsonl"
-testing_snli ="/home/ioannis/data/multinli_0.9/multinli_0.9_dev_mismatched.jsonl"
+# training_snli ="/home/ioannis/data/multinli_0.9/multinli_0.9_train.jsonl"
+# validation_snli ="/home/ioannis/data/multinli_0.9/multinli_0.9_dev_matched.jsonl"
+# testing_snli ="/home/ioannis/data/multinli_0.9/multinli_0.9_dev_mismatched.jsonl"
 
 # training_snli = "/home/ioannis/data/snli_1.0/snli_1.0_train.jsonl"
 # validation_snli = "/home/ioannis/data/snli_1.0/snli_1.0_dev.jsonl"
-# testing_snli = "/home/ioannis/data/snli_1.0/snli_1.0_test.jsonl"
-files = [training_snli, validation_snli, testing_snli]
+testing_snli = "/home/ioannis/data/snli_1.0/snli_1.0_test.jsonl"
+# files = [training_snli, validation_snli, testing_snli]
+files = [testing_snli]
 all_snli = []
 for snli_file in files:
     with open(snli_file, 'r') as file_:
@@ -29,7 +30,7 @@ for snli_file in files:
             json_buffer = {'des':sen1, 'web':sen2, 'class':label}
             all_snli.append(json_buffer)
 
-with open('/home/ioannis/data/all_binary_multi_snli.json', 'w') as file_:
+with open('/home/ioannis/data/testing_binary_snli_snli.json', 'w') as file_:
     for json_buffer in all_snli:
         write_json_line(json_buffer, file_)
 
