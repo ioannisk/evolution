6cfc349c1c321d462dc66f1ee0612be320d97531
ioannisk <john.karamanlakis@gmail.com>
2017-08-02 16:58:45 +0100

    working on sl evaluation

--- code/filter_generation.py
+++ code/filter_generation.py
@@ -14,8 +14,8 @@ import re
 N = 3
 MAX_LEN= 120
 supervised_validation_volume = 8000
-# MAX_DES_LEN=MAX_LEN
-# MAX_WEB_LEN=MAX_LEN
+MAX_DES_LEN=MAX_LEN
+MAX_WEB_LEN=MAX_LEN
 data_path = "../data/1rfolds{}_sl_filtered/".format(N)
 # data_path = "../data/10rfolds{}/".format(N)
 # data_path = "../data/100rfolds{}/".format(N)
