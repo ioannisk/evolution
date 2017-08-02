7e4dd58f75e74550294db1e83cbf3397a5c1fe23
ioannisk <john.karamanlakis@gmail.com>
2017-08-02 10:56:53 +0100

    work on various datasets

--- code/fold_analysis.py
+++ code/fold_analysis.py
@@ -9,12 +9,17 @@ data_path = "/home/ioannis/evolution/data/{}/".format(choosen_fold)
 # for li
 for i in range(3):
     with open(data_path+"fold{}/training.json".format(i),"r") as file_:
+        print("Analysing fold {}".format(i))
+        counter = 0
         for line in file_:
+            if counter % 100000:
+                print(counter)
             line = line.strip()
             line = json.load(line)
             des = line["des"]
             web = line["web"]
-            if len(des.split())==0 orlen(web.split())==0:
+            counter +=1
+            if len(des.split())==0 or len(web.split())==0:
                 print(line[web_id])
 
 
