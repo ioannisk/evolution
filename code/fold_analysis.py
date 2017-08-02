9ad65cf1cda1d4b704f37a6dd3ed9b801fcc75dd
ioannisk <john.karamanlakis@gmail.com>
2017-08-02 11:26:34 +0100

    work on various datasets

--- code/fold_analysis.py
+++ code/fold_analysis.py
@@ -8,19 +8,22 @@ data_path = "/home/ioannis/evolution/data/{}/".format(choosen_fold)
 
 # for li
 for i in range(3):
-    with open(data_path+"fold{}/supervised_validation.json".format(i),"r") as file_:
+    with open(data_path+"fold{}/validation.json".format(i),"r") as file_:
         print("Analysing fold {}".format(i))
         counter = 0
+        used_classes = set()
         for line in file_:
             if counter % 100000 == 0:
                 print(counter)
             line = line.strip()
             line = json.loads(line)
-            des = line["des"]
-            web = line["web"]
-            counter +=1
+            # des = line["des"]
+            # web = line["web"]
+            class_= ["web_class"]
+            used_classes.add(class_)
             if len(des.split())==0 or len(web.split())==0:
                 print(line[web_id])
+            print(used_classes)
 
 
 
