79b03aba913c39151555a8c27e79927df2eca3b8
ioannisk <john.karamanlakis@gmail.com>
2017-08-17 22:39:27 +0100

    evaluation change

---
+++ code/add_noise.py
@@ -0,0 +1,5 @@
+# 1. add noise to description 20 %
+# 1. add noise to description 50 %
+# 2. remove all keywords of descriptions from websites
+# 3. add noise to both 20 %
+# 4. add noise to both 50 %
