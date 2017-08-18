717bba9dc2d4047354f66ff7685dd97572a7de4c
ioannisk <john.karamanlakis@gmail.com>
2017-08-18 14:10:30 +0100

    update

--- code/add_noise.py
+++ code/add_noise.py
@@ -105,6 +105,7 @@ def filter_data(files):
 if __name__=="__main__":
     data_path = "/home/ioannis/data/recovery_test/"
     files =[data_path +"fold{}/".format(i)+"ranking_validation.json" for i in range(0,3)]
+    files = files[:1]
     filter_data(files)
     # web_noise(files)
     # vocab_overlap(files)
