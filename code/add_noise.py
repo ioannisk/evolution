8ce85faad313430c7882032fb63ee974e45268c3
ioannisk <john.karamanlakis@gmail.com>
2017-08-18 10:17:20 +0100

    update

--- code/add_noise.py
+++ code/add_noise.py
@@ -23,6 +23,7 @@ def des_vocabulary(file_str):
             line = line.strip()
             line = json.loads(line)
             des_list = line['des'].split()
+            print("---")
             print(len(des_list))
             des_list = sample(des_list, 0.4)
             print(len(des_list))
