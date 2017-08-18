b0ad21ab3154097843e0bf6ac1998a27bc5c1a22
ioannisk <john.karamanlakis@gmail.com>
2017-08-18 09:42:23 +0100

    update

--- code/add_noise.py
+++ code/add_noise.py
@@ -1,5 +1,17 @@
-# 1. add noise to description 20 %
-# 1. add noise to description 50 %
+# 1. add noise to web 20 %
+# 1. add noise to web 50 %
 # 2. remove all keywords of descriptions from websites
 # 3. add noise to both 20 %
 # 4. add noise to both 50 %
+
+# def web_noise():
+
+
+
+
+
+if __name__=="__main__":
+    data_path = "/home/ioannis/data/recovery_test/"
+    files =[data_path +"fold{}/".format(i)+"ranking_validation.json" for i in range(0,3)]
+    print(files)
+
