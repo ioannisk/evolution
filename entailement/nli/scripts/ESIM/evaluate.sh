e98878ae29fbe33593db6a1f4184a53845a87cf5
ioannisk <john.karamanlakis@gmail.com>
2017-08-19 19:42:57 +0100

    update

--- entailement/nli/scripts/ESIM/evaluate.sh
+++ entailement/nli/scripts/ESIM/evaluate.sh
@@ -6,7 +6,7 @@ export THEANO_FLAGS='mode=FAST_RUN,device=gpu0,floatX=float32,optimizer_includin
 # CPU
 # export THEANO_FLAGS='mode=FAST_RUN,device=cpu,floatX=float32'
 
-nohup python -u ./evaluate.py model_1 1 > log1.txt 2>&1 &
+nohup python -u ./evaluate.py model_2 2 > log2.txt 2>&1 &
 #python ./train.py
 
 
