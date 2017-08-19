c3bb65940da17d5490f87c8737b7adeedbc411cb
ioannisk <john.karamanlakis@gmail.com>
2017-08-19 15:24:52 +0100

    update

--- code/txt_stats.py
+++ code/txt_stats.py
@@ -1,11 +1,14 @@
 import json
-vocab = set()
-with open("/home/ioannis/data/snli_1.0/snli_1.0_train.jsonl", "r") as file_:
-    for line in file_:
-        line = json.loads(line.strip())
-        sen1 = line['sentence1']
-        sen2 = line['sentence2']
-        data = sen1 + " "+ sen2
-        for word in data.split():
-            vocab.add(word)
-print(len(vocab))
+import sys
+
+print(sys.argv)
+# vocab = set()
+# with open("/home/ioannis/data/snli_1.0/snli_1.0_train.jsonl", "r") as file_:
+#     for line in file_:
+#         line = json.loads(line.strip())
+#         sen1 = line['sentence1']
+#         sen2 = line['sentence2']
+#         data = sen1 + " "+ sen2
+#         for word in data.split():
+#             vocab.add(word)
+# print(len(vocab))
--- entailement/nli/scripts/ESIM/web_train.py
+++ entailement/nli/scripts/ESIM/web_train.py
@@ -1,10 +1,13 @@
 import numpy
 import os
-
+import sys
 from main import train
 
 if __name__ == '__main__':
-    model_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+    # model_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+    model_name = sys.argv[1]
+    training_data_path = sys.argv[2]
+    validation_data_path = sys.argv[3]
     train(
     saveto           = './{}.npz'.format(model_name),
     reload_          = False,
@@ -15,7 +18,7 @@ if __name__ == '__main__':
     decay_c          = 0.,
     clip_c           = 10.,
     lrate            = 0.0004,
-    optimizer        = 'adam', 
+    optimizer        = 'adam',
     maxlen           = 100,
     batch_size       = 32,
     valid_batch_size = 32,
@@ -24,15 +27,15 @@ if __name__ == '__main__':
     saveFreq         = int(1500),
     use_dropout      = True,
     verbose          = False,
-    datasets         = ['../../data/word_sequence/premise_snli_1.0_train.txt', 
+    datasets         = ['../../data/word_sequence/premise_snli_1.0_train.txt',
                         '../../data/word_sequence/hypothesis_snli_1.0_train.txt',
                         '../../data/word_sequence/label_snli_1.0_train.txt'],
-    valid_datasets   = ['../../data/word_sequence/premise_snli_1.0_dev.txt', 
+    valid_datasets   = ['../../data/word_sequence/premise_snli_1.0_dev.txt',
                         '../../data/word_sequence/hypothesis_snli_1.0_dev.txt',
                         '../../data/word_sequence/label_snli_1.0_dev.txt'],
-    test_datasets    = ['../../data/word_sequence/premise_snli_1.0_test.txt', 
-                        '../../data/word_sequence/hypothesis_snli_1.0_test.txt',
-                        '../../data/word_sequence/label_snli_1.0_test.txt'],
+    # test_datasets    = ['../../data/word_sequence/premise_snli_1.0_test.txt',
+    #                     '../../data/word_sequence/hypothesis_snli_1.0_test.txt',
+    #                     '../../data/word_sequence/label_snli_1.0_test.txt'],
     dictionary       = '../../data/word_sequence/vocab_cased.pkl',
     embedding        = '../../data/glove/glove.840B.300d.txt',
     )
