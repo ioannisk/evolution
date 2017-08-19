03b1f83051746cb254e31729651d227a382ec792
ioannisk <john.karamanlakis@gmail.com>
2017-08-19 18:45:42 +0100

    update

--- entailement/nli/scripts/ESIM/web_train.sh
+++ entailement/nli/scripts/ESIM/web_train.sh
@@ -6,7 +6,7 @@ export THEANO_FLAGS='mode=FAST_RUN,device=gpu0,floatX=float32,optimizer_includin
 # CPU
 # export THEANO_FLAGS='mode=FAST_RUN,device=cpu,floatX=float32'
 
-nohup python -u ./web_train.py model_1 1 > log.txt 2>&1 &
+nohup python -u ./web_train.py model_2 2 > log.txt 2>&1 &
 #python ./train.py
 
 
