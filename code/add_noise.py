68cafff14e7f4e74974ce1931a1b6b6ec99c77fb
ioannisk <john.karamanlakis@gmail.com>
2017-08-20 00:40:23 +0100

    new evaluation

--- code/add_noise.py
+++ code/add_noise.py
@@ -64,7 +64,7 @@ def vocab_overlap(files):
 
 
 def web_noise(files):
-    noise = 0.6
+    noise = 0.8
     print(noise)
     for file_str in files:
         print("writing file {}".format(file_str))
