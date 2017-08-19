e3c64943a19ac41b22d91d6794899506b51a6af0
ioannisk <john.karamanlakis@gmail.com>
2017-08-19 18:26:05 +0100

    update

--- code/noisy_evaluation.py
+++ code/noisy_evaluation.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import random
 
 RANKS = list(range(1,201))
-NOISE = 0.2
+NOISE = 0.4
 # choosen_fold = "1rfolds3"
 # choosen_model = "1rfolds3_1"
 # data_path = "/home/ioannis/evolution/data/{}/".format(choosen_fold)
