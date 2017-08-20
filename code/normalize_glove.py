fdbbd2f3da2a8fb53a7075ca80a7e33847476768
ioannisk <john.karamanlakis@gmail.com>
2017-08-20 14:27:05 +0100

    some modification (untested) on DA and l2 norm for glove

--- code/normalize_glove.py
+++ code/normalize_glove.py
@@ -23,11 +23,9 @@ def load_text_embeddings(path):
             word = fields[0]
             words.append(word)
             vector = np.array([float(x) for x in fields[1:]], dtype=np.float32)
-            print vector.shape
             norm = (np.linalg.norm(vector))
             vector = vector /norm
             print np.linalg.norm(vector)
-            advfvfv
             vectors.append(vector)
     # IPython.embed()
     embeddings = np.array(vectors, dtype=np.float32)
