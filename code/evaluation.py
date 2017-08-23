240dc82381777a76fed68d59730114ec0603a6e6
ioannisk <john.karamanlakis@gmail.com>
2017-08-23 14:14:18 +0100

    addinng eval eda

--- code/evaluation.py
+++ code/evaluation.py
@@ -40,8 +40,8 @@ RANKS = list(range(1,MAX_RANK))
 choosen_fold = "recovery_test"
             # choosen_model = "best_models_1rfold3_sl"
             # choosen_fold = "recovery_test"
-# choosen_model ="eda_models"
-choosen_model = "recovery_test"
+choosen_model ="eda_models"
+# choosen_model = "recovery_test"
 data_file = "validation"
 data_path = "/home/ioannis/data/{}/".format(choosen_fold)
 
