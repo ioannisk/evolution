b8b5e6cea2fc60093dffc4fbba7e9d1109cac7cc
ioannisk <john.karamanlakis@gmail.com>
2017-08-24 16:46:33 +0100

    ensemble in the makings

--- code/make_ensemble.py
+++ code/make_ensemble.py
@@ -4,18 +4,16 @@ models = ["eda_models", "eda_models_1"]
 folds = [0,1,2,3,4]
 
 for fold in folds:
+    print("ensemble {}".format(fold))
     num_lines = sum(1 for line in open('/home/ioannis/models/eda_models/model{}/prob_predictions.txt'.format(fold), 'r'))
-    print(num_lines)
     store_pred = np.zeros(num_lines)
-
-    # for model in models:
-    #     with open('/home/ioannis/models/{}/model{}/prob_predictions.txt'.format(model, fold), 'r') as file_:
-    #         for i, line in enumerate(file_):
-    #             line=line.strip()
-    #             line=float(line)
-    #             store_pred[i] += line
-    # store_pred = store_pred/len(models)
-    # output = open('/home/ioannis/models/ensemble/model{}/prob_predictions.txt'.format(fold), 'w')
-    # for i in store_pred:
-    #     output.write("{}\n".format(i))
+    for model in models:
+        with open('/home/ioannis/models/{}/model{}/prob_predictions.txt'.format(model, fold), 'r') as file_:
+            for i, line in enumerate(file_):
+                line=float(line)
+                store_pred[i] += line
+    store_pred = store_pred/len(models)
+    output = open('/home/ioannis/models/ensemble/model{}/prob_predictions.txt'.format(fold), 'w')
+    for i in store_pred:
+        output.write("{}\n".format(i))
 
