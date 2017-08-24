e68f4c0d00e2768f66d7485cfcd419edaa659f00
ioannisk <john.karamanlakis@gmail.com>
2017-08-24 16:47:41 +0100

    ensemble in the makings

--- code/make_ensemble.py
+++ code/make_ensemble.py
@@ -8,10 +8,12 @@ for fold in folds:
     num_lines = sum(1 for line in open('/home/ioannis/models/eda_models/model{}/prob_predictions.txt'.format(fold), 'r'))
     store_pred = np.zeros(num_lines)
     for model in models:
+        print("    reading model {}".format(model)))
         with open('/home/ioannis/models/{}/model{}/prob_predictions.txt'.format(model, fold), 'r') as file_:
             for i, line in enumerate(file_):
                 line=float(line)
                 store_pred[i] += line
+    print("    Writting")
     store_pred = store_pred/len(models)
     output = open('/home/ioannis/models/ensemble/model{}/prob_predictions.txt'.format(fold), 'w')
     for i in store_pred:
