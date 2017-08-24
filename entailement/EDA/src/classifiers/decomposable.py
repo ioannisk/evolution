c3c2346e14fa3c17700d820c75c270b6067bf2ba
ioannisk <john.karamanlakis@gmail.com>
2017-08-24 16:59:25 +0100

    removed max pool

--- entailement/EDA/src/classifiers/decomposable.py
+++ entailement/EDA/src/classifiers/decomposable.py
@@ -249,7 +249,8 @@ class DecomposableNLIModel(object):
         Return the number of units used by the network when computing
         the aggregated representation of the two sentences.
         """
-        return 2 * self.num_units + self.num_units + self.num_units
+        # return 2 * self.num_units + self.num_units + self.num_units
+        return 2 * self.num_units
 
     def _apply_feedforward(self, inputs, num_input_units, scope,
                            reuse_weights=False, initializer=None,
@@ -337,16 +338,13 @@ class DecomposableNLIModel(object):
         v2 = mask_3d(v2, self.sentence2_size, 0, 1)
 
 
-        # tf.argmax(tf.reduce_sum(tf.multiply(v1,v1),[2]),[1])
-        # tf.argmax(tf.reduce_sum(tf.multiply(v2,v2),[2]),[1])
-        # v1_norms = tf.norm(v1, axis=2)
-        # v2_norms = tf.norm(v2, axis=2)
-        max_pool_v1 = tf.reduce_max(v1, 1)
-        max_pool_v2 = tf.reduce_max(v2, 1)
+        # max_pool_v1 = tf.reduce_max(v1, 1)
+        # max_pool_v2 = tf.reduce_max(v2, 1)
 
         v1_sum = tf.reduce_mean(v1, [1])
         v2_sum = tf.reduce_mean(v2, [1])
-        return tf.concat(axis=1, values=[v1_sum, v2_sum, max_pool_v1, max_pool_v2])
+        # return tf.concat(axis=1, values=[v1_sum, v2_sum, max_pool_v1, max_pool_v2])
+        return tf.concat(axis=1, values=[v1_sum, v2_sum])
 
     def attend(self, sent1, sent2):
         """
