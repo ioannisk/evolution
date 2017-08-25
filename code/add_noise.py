62da85e9dc476b5d0570047270f9b893a7ec122b
ioannisk <john.karamanlakis@gmail.com>
2017-08-25 10:31:43 +0100

    removed max pool

--- code/add_noise.py
+++ code/add_noise.py
@@ -100,11 +100,42 @@ def filter_data(files):
                 write_json_line(line, output)
 
 
-if __name__=="__main__":
+#####
+# What to do :
+#
+# Read training, and see what classes you use, then sample one more negative
+#
+####
+def add_negative_data():
+    class_descriptions = read_descriptions()
+    companies_descriptions= read_meta()
+    class_descriptions, companies_descriptions = web_des_intersection(class_descriptions, companies_descriptions)
     data_path = "/home/ioannis/data/recovery_test/"
-    files =[data_path +"fold{}/".format(i)+"ranking_validation.json" for i in range(0,5)]
+    files =[data_path +"fold{}/".format(i)+"training.json" for i in range(0,5)]
+    for file_str in files:
+        output = open(file_str + ".ratio2", 'w')
+        with open(file_str, 'r') as file_:
+            for line in file_:
+                line = line.strip()
+                line = json.loads(line)
+                if line["class"] == "entailment":
+                    write_json_line(line, output)
+                else:
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    # data_path = "/home/ioannis/data/recovery_test/"
+    # files =[data_path +"fold{}/".format(i)+"ranking_validation.json" for i in range(0,5)]
     # print(files)
     # filter_data(files)
     # web_noise(files)
     # vocab_overlap(files)
+
+    add_negative_data()
 
