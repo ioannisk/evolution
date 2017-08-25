6f1a33e3bea1e15c97bf30aee3fcc84cd036a515
ioannisk <john.karamanlakis@gmail.com>
2017-08-25 15:29:10 +0100

    removed max pool

--- entailement/EDA/src/night_train_2.sh
+++ entailement/EDA/src/night_train_2.sh
@@ -3,15 +3,16 @@
 ### Have not run it yet ###
 ### need to run this tomorrow morning
 
-# python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold1/training.json /home/ioannis/data/recovery_test/fold1/validation.json /home/ioannis/models/eda_models/model1 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.02 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
+python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold0/training.json /home/ioannis/data/recovery_test/fold0/validation.json /home/ioannis/models/eda_models/model0 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.02 --report 100000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
+python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold1/training.json /home/ioannis/data/recovery_test/fold1/validation.json /home/ioannis/models/eda_models/model1 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.02 --report 100000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
 # python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold2/training.json /home/ioannis/data/recovery_test/fold2/validation.json /home/ioannis/models/eda_models/model2 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.02 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
 # python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold3/training.json /home/ioannis/data/recovery_test/fold3/validation.json /home/ioannis/models/eda_models/model3 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.02 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
 # python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold4/training.json /home/ioannis/data/recovery_test/fold4/validation.json /home/ioannis/models/eda_models/model4 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.02 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
 
 
-python -u my_evaluate.py /home/ioannis/models/eda_models/model1 /home/ioannis/data/recovery_test/fold1/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
-python -u my_evaluate.py /home/ioannis/models/eda_models/model2 /home/ioannis/data/recovery_test/fold2/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
-python -u my_evaluate.py /home/ioannis/models/eda_models/model3 /home/ioannis/data/recovery_test/fold3/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
-python -u my_evaluate.py /home/ioannis/models/eda_models/model4 /home/ioannis/data/recovery_test/fold4/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
+# python -u my_evaluate.py /home/ioannis/models/eda_models/model1 /home/ioannis/data/recovery_test/fold1/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
+# python -u my_evaluate.py /home/ioannis/models/eda_models/model2 /home/ioannis/data/recovery_test/fold2/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
+# python -u my_evaluate.py /home/ioannis/models/eda_models/model3 /home/ioannis/data/recovery_test/fold3/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
+# python -u my_evaluate.py /home/ioannis/models/eda_models/model4 /home/ioannis/data/recovery_test/fold4/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
 
 # evaluation running atm
