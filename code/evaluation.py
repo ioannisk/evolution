52df5420ec8d3d2fb3c9cb2def16b674629f21ca
ioannisk <john.karamanlakis@gmail.com>
2017-08-26 22:28:37 +0100

    removed max pool

--- code/evaluation.py
+++ code/evaluation.py
@@ -52,9 +52,9 @@ data_path = "/home/ioannis/data/{}/".format(choosen_fold)
 #
 # folds = [0,1,2,3,4,5,6,14,15,16]
 # folds = [0,1]
-# folds = [0,1,2,3,4]
+folds = [0,1,2,3,4]
 # folds = [3,4]
-folds = [1]
+# folds = [1]
 # folds = [14]
 # folds = [0,2,4]
 class_descriptions = read_descriptions()
