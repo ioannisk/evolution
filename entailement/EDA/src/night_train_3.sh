15d2b6b997ef0e89745049b918575dc52646b616
ioannisk <john.karamanlakis@gmail.com>
2017-08-27 17:32:54 +0100

    removed max pool

--- entailement/EDA/src/night_train_3.sh
+++ entailement/EDA/src/night_train_3.sh
@@ -1,18 +1,15 @@
 
 
-### Have not run it yet ###
-### need to run this tomorrow morning
-
-python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold1/training.json /home/ioannis/data/recovery_test/fold0/validation.json /home/ioannis/models/eda_models_2/model0 mlp --lower -e 5 -u 200 -d 0.8  -b 32 -r 0.05 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
-python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold1/training.json /home/ioannis/data/recovery_test/fold1/validation.json /home/ioannis/models/eda_models_2/model1 mlp --lower -e 5 -u 200 -d 0.8  -b 32 -r 0.05 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
-# python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold2/training.json /home/ioannis/data/recovery_test/fold2/validation.json /home/ioannis/models/eda_models_2/model2 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.05 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
-# python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold3/training.json /home/ioannis/data/recovery_test/fold3/validation.json /home/ioannis/models/eda_models_2/model3 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.05 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
-# python -u my_train.py ~/data/glove/glove-840B_l2.npy /home/ioannis/data/recovery_test/fold4/training.json /home/ioannis/data/recovery_test/fold4/validation.json /home/ioannis/models/eda_models_2/model4 mlp --lower -e 4 -u 200 -d 0.8  -b 32 -r 0.05 --report 60000 --vocab ~/data/glove/glove-840B-vocabulary_l2.txt
+# nohup python -u my_evaluate.py /home/ioannis/models/eda_models_2/model1 /home/ioannis/data/recovery_test/fold1/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt &
 
 
-# python -u my_evaluate.py /home/ioannis/models/eda_models_2/model1 /home/ioannis/data/recovery_test/fold1/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
-# python -u my_evaluate.py /home/ioannis/models/eda_models_2/model2 /home/ioannis/data/recovery_test/fold2/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
-# python -u my_evaluate.py /home/ioannis/models/eda_models_2/model3 /home/ioannis/data/recovery_test/fold3/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
-# python -u my_evaluate.py /home/ioannis/models/eda_models_2/model4 /home/ioannis/data/recovery_test/fold4/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt
+#### Verify that you know how to replicate the predictions: It is important for the noisy predictions
 
-# evaluation running atm
+
+
+python -u my_evaluate.py /home/ioannis/models/reproduced/model0 /home/ioannis/data/recovery_test/fold0/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions_rep.txt
+
+# nohup python -u my_evaluate.py /home/ioannis/models/eda_models/model1 /home/ioannis/data/recovery_test/fold1/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt > log1.txt &
+# nohup python -u my_evaluate.py /home/ioannis/models/eda_models/model2 /home/ioannis/data/recovery_test/fold2/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt > log2.txt &
+# nohup python -u my_evaluate.py /home/ioannis/models/eda_models/model3 /home/ioannis/data/recovery_test/fold3/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt > log3.txt &
+# nohup python -u my_evaluate.py /home/ioannis/models/eda_models/model4 /home/ioannis/data/recovery_test/fold4/ranking_validation.json ~/data/glove/glove-840B_l2.npy --vocabulary ~/data/glove/glove-840B-vocabulary_l2.txt -save prob_predictions.txt > log4.txt &
