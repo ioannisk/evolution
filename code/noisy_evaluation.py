e4195efdab742ba21e96310c8325964dd8b51536
ioannisk <john.karamanlakis@gmail.com>
2017-08-28 11:12:18 +0100

    night scripts

--- code/noisy_evaluation.py
+++ code/noisy_evaluation.py
@@ -3,10 +3,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from nltk.corpus import stopwords
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from collections import Counter,defaultdict
+from sklearn.decomposition import LatentDirichletAllocation
 # from generation_matching_dataset import read_descriptions, read_meta, web_des_intersection
 from new_generation_matching_dataset import read_descriptions, read_meta, web_des_intersection
 import matplotlib.pyplot as plt
