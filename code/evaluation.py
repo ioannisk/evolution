6d1956993f425df8d950cd528c1152e6f4d543ad
ioannisk <john.karamanlakis@gmail.com>
2017-08-28 16:31:44 +0100

    test quick validation

--- code/evaluation.py
+++ code/evaluation.py
@@ -55,10 +55,10 @@ data_path = "/home/ioannis/data/{}/".format(choosen_fold)
 # Comparison on folds 2, 4, 0
 #
 # folds = [0,1,2,3,4,5,6,14,15,16]
-folds = [0,1,2,3,4]
+# folds = [0,1,2,3,4]
 # folds = [0,1,2,3,4]
 # folds = [0,1,2,3]
-# folds = [1]
+folds = [1, 2]
 # folds = [14]
 # folds = [0,2,4]
 class_descriptions = read_descriptions()
