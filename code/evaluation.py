848679b595338651509dbf61ae7450a4688cf0cf
ioannisk <john.karamanlakis@gmail.com>
2017-08-30 18:31:16 +0100

    eval

--- code/evaluation.py
+++ code/evaluation.py
@@ -15,10 +15,10 @@ from sklearn.decomposition import LatentDirichletAllocation
 from new_generation_matching_dataset import read_descriptions, read_meta, web_des_intersection
 import matplotlib.pyplot as plt
 
-print("Loading Word2Vec")
-from gensim.models import Word2Vec
-model_w2v = Word2Vec.load_word2vec_format('/home/ioannis/scp/GoogleNews-vectors-negative300.bin',binary=True)
-model_w2v_vocab = model_w2v.vocab
+# print("Loading Word2Vec")
+# from gensim.models import Word2Vec
+# model_w2v = Word2Vec.load_word2vec_format('/home/ioannis/scp/GoogleNews-vectors-negative300.bin',binary=True)
+# model_w2v_vocab = model_w2v.vocab
 import nltk
 from nltk.corpus import stopwords
 stopwords = nltk.corpus.stopwords.words('english')
