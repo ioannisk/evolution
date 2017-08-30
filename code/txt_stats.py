4288150b0f136eab11eb130fc7178f357ce75fa6
ioannisk <john.karamanlakis@gmail.com>
2017-08-30 17:53:59 +0100

    txt filter

--- code/txt_stats.py
+++ code/txt_stats.py
@@ -44,8 +44,13 @@ def write_json_line(json_ ,file_):
 ## This code makes a 1 shot validation
 # CHOOSE_MODEL = "best_eda"
 CHOOSE_MODEL = "filtered_models_2"
-CHOOSEN_FOLD = 4
-
+CHOOSEN_FOLD = 0
+fold = {0:[61200,22110,14132,21200,52101,58210],
+        1:[46341,13950,74203,20412,52102,82190],
+        2:[28120,81223,31030,14390,20150],
+        3:[28410,18130,10840,28120,46360,26514],
+        4:[23320,65300,46210,13910,10831,63910]
+}
 with open("/home/ioannis/models/{}/model{}/prob_predictions_filter.txt".format(CHOOSE_MODEL,CHOOSEN_FOLD), "r") as file_:
     predictions = []
     for line in file_:
@@ -57,11 +62,11 @@ with open("/home/ioannis/data/recovery_test/fold{}/ranking_validation.json.filte
     for line in file_:
         line = json.loads(line.strip())
         data.append(line)
-# valid_subset = open("/home/ioannis/data/recovery_test/fold{}/ranking_validation.json_valid".format(CHOOSEN_FOLD), 'w')
-# valid_subset_pred = open("/home/ioannis/models/{}/model{}/prob_predictions_valid.txt".format(CHOOSE_MODEL,CHOOSEN_FOLD), 'w')
+valid_subset = open("/home/ioannis/data/recovery_test/fold{}/ranking_validation.json_valid".format(CHOOSEN_FOLD), 'w')
+valid_subset_pred = open("/home/ioannis/models/{}/model{}/prob_predictions_valid.txt".format(CHOOSE_MODEL,CHOOSEN_FOLD), 'w')
 
-# testing_subset = open("/home/ioannis/data/recovery_test/fold{}/ranking_validation.json_test".format(CHOOSEN_FOLD), 'w')
-# testing_subset_pred = open("/home/ioannis/models/{}/model{}/prob_predictions_test.txt".format(CHOOSE_MODEL,CHOOSEN_FOLD), 'w')
+testing_subset = open("/home/ioannis/data/recovery_test/fold{}/ranking_validation.json_test".format(CHOOSEN_FOLD), 'w')
+testing_subset_pred = open("/home/ioannis/models/{}/model{}/prob_predictions_test.txt".format(CHOOSE_MODEL,CHOOSEN_FOLD), 'w')
 classes = defaultdict(list)
 for i in range(0, len(data), 556):
     datapoint = data[i:i+556]
@@ -69,33 +74,28 @@ for i in range(0, len(data), 556):
     web_id = datapoint[0]["web_id"]
     web_class = datapoint[0]["web_class"]
     classes[web_class].append(zip(datapoint, datapoint_pred))
-
-
-for cl in classes:
-    print(cl, len(classes[cl]))
-
-
-
-# fold2 = [28120,81223,31030,14390,20150]
-# fold1 = [46341,13950,74203,20412,52102,82190]
 # for cl in classes:
-#     if int(cl) in fold1:
-#         for datapoints in classes[cl]:
-#             for d, pred in datapoints:
-#                 # write_json_line(d, valid_subset)
-#                 valid_subset_pred.write("{}\n".format(pred))
-#     else:
-#         for datapoints in classes[cl]:
-#             for d, pred in datapoints:
-#                 # write_json_line(d, testing_subset)
-#                 testing_subset_pred.write("{}\n".format(pred))
-
-
-
-# fold0 = [61200,22110,14132,21200,52101,58210]
-# fold1 = [46341,13950,74203,20412,52102,82190]
+#     print(cl, len(classes[cl]))
 # fold2 = [28120,81223,31030,14390,20150]
-# fold3 = [28410,18130,10840,28120,46360,26514]
+# fold1 = [46341,13950,74203,20412,52102,82190]
+for cl in classes:
+    if int(cl) in fold[CHOOSEN_FOLD]:
+        for datapoints in classes[cl]:
+            for d, pred in datapoints:
+                write_json_line(d, valid_subset)
+                valid_subset_pred.write("{}\n".format(pred))
+    else:
+        for datapoints in classes[cl]:
+            for d, pred in datapoints:
+                write_json_line(d, testing_subset)
+                testing_subset_pred.write("{}\n".format(pred))
+
+
+# fold0 =
+# fold1 =
+# fold2 =
+# fold3 =
+# fold4 =
 
 
 
